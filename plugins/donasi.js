7f9af000e9b4e5279ca341a34438aa7c7e1c7d40
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-03-24 13:15:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ilmanhdyt
-├ Telkomsel:081351047727
+├ Dana:082365089294
+├ Telkomsel:082365089294
 └────
 `.trim(), '© SHIRAORI', 'Pemilik Bot', '.owner', m)
 handler.help = ['donasi']
