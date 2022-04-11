08d5225b8acaddd8efccaa4b2b1e459d6f41a01e
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-04-11 21:36:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -34,6 +34,7 @@ global.APIKeys = { // APIKey nya disini
 // Sticker WM
 global.packname = 'created by'
 global.author = '@frmndaa'
+
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu..*_'
 global.eror = '*_Server Eror_*'
