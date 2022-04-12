219a7917bb75d2f4e8348f0d3ec4eca2678c3354
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-04-12 19:44:00 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,11 +1,11 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6282159536790', 'Ido', m)
+  this.sendContact(m.chat, '6282365089294', 'firman☝️😅', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
 
-module.exports = handler
+module.export = handler
